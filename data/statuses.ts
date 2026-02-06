5aaef84f298751eeee4a7004745f6c2e413d3f87
Faizan Adil <faizanibneadil1@google.com>
2026-02-06 14:16:01 +0500

    move component from app to components

---
+++ data/statuses.ts
@@ -0,0 +1,8 @@
+export const statuses = [
+    { color: '#8597A8', status: 'Not Started' },
+    { color: '#F59F0A', status: 'In Progress' },
+    { color: '#1EA54E', status: 'Completed' },
+    { color: '#004479', status: 'Partially Uploaded' },
+    { color: '#0078D7', status: 'Fully Uploaded' },
+    { color: '#F50A0A', status: 'Delayed' },
+]
